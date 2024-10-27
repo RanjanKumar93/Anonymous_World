7f1166f149e94ca34037e272ba6ef2cdebd4a3bc
RanjanKumar93 <rk0936626@gmail.com>
2024-10-28 00:02:22 +0530

    add to gitignore2

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 // import 'package:go_router/go_router.dart';
 import 'firebase_options.dart';
 
@@ -15,7 +15,7 @@ FirebaseFirestore db = FirebaseFirestore.instance;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
+  // await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
